36abdded6b4e4e97e7e6eb2489b1c5b531254140
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2026-04-29 14:22:04 +0800

    Preparing for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -21,6 +21,10 @@ app.use("/api/deliveries", require("./routes/deliveryRoutes"));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
