b25fec3e6969099f6a7acb914acefc535380cda3
Chieyo <fvalencia@gbox.adnu.edu.ph>
2026-05-12 21:53:51 +0800

    fix (frontend): fix syntax

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body suppressHydrationWarning={true}>
+    <html lang="en">
+      <body>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
