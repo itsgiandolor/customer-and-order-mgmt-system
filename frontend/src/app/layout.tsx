a097cff2fd10aa4b9769c14a5271dd426f5bfc7a
Chieyo <fvalencia@gbox.adnu.edu.ph>
2026-05-13 08:04:56 +0800

    fix: resolve Shadow DOM error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <CartProvider>{children}</CartProvider>
       </body>
     </html>
